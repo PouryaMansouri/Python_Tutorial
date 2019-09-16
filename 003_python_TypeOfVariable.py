ed70b2ac6b8de8116ecaeb716be1fce583448dce
pourya <pourya.mansouri@hotmail.com>
2019-09-16 09:42:20 +0430

    003_type

---
+++ 003_python_TypeOfVariable.py
@@ -0,0 +1,19 @@
+#003_python_TypeOfVariable.py
+# @author Pourya_mansouri1 
+# different data types
+# in the same variable
+a = 22
+print(a)
+print(type(a))
+
+a= 'string'
+print(a)
+print(type(a))
+
+a = 2.4
+print(a)
+print(type(a))
+
+
+
+
