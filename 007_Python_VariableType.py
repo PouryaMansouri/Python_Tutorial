0cd84870c61de96c17e873502666dc972ab6f97a
pourya <pourya.mansouri@hotmail.com>
2019-09-16 13:30:13 +0430

    VariableTpyes

---
+++ 007_Python_VariableType.py
@@ -0,0 +1,16 @@
+# 007_Python_VariableType.py
+# @pourya_mansouiri1
+# Python has different variables types
+
+int_var = 22
+str_var = 'Hello, world!'
+float_var = 2.2
+list_var = [1,2,3,4,5]
+dict_var = {'x': 100 , 'y':'hello'}
+
+
+print(int_var,'type is -->',type(int_var))
+print(str_var,'type is -->',type(str_var))
+print(float_var,'type is -->',type(float_var))
+print(list_var,'type is -->',type(list_var))
+print(dict_var,'type is -->',type(dict_var))
