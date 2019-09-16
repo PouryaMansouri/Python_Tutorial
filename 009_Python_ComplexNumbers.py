c5c51085b5be625a5544108901687746939f3a6b
pourya <pourya.mansouri@hotmail.com>
2019-09-16 14:45:16 +0430

    ComplexNumbers

---
+++ 009_Python_ComplexNumbers.py
@@ -0,0 +1,12 @@
+# 009_Python_ComplexNumbers.py
+# @pourya_mansouiri1
+# Using Complex Numbers
+
+x = 7+6j # not use i
+y = 9-7j
+
+# Print distance between 
+# these two points
+
+print(abs(x - y ))
+
