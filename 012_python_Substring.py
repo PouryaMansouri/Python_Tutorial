0a75dfd172c4ded03ca30e4325766fc4bf086a83
pourya <pourya.mansouri@hotmail.com>
2019-09-23 08:47:55 +0330

    SubString

---
+++ 012_python_Substring.py
@@ -0,0 +1,15 @@
+#012_python_Substring.py 
+# @pourya_mansouiri1
+
+string = 'Hello, World!'
+#         0123456789...
+#          ^^^^^
+
+# I want to print "ello"
+# GEt the characters from position 1 to
+#positon 5(not included)
+
+Isolated_characters = string[1:5]
+
+print(Isolated_characters)
+
