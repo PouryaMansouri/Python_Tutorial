849ebf673a9af905a8d7b068691d524a12681525
pourya <pourya.mansouri@hotmail.com>
2019-09-23 08:13:56 +0330

    Len function

---
+++ 010_python_len.py
@@ -0,0 +1,12 @@
+#010_python_len.py 
+# @pourya_mansouiri1
+# The "len" is a built-in function
+
+
+list_var = [1,2,3,4,5,6]
+str_var = "Hellow,world!"
+dict_var = {'x' : 'hello', 'y' : 120}
+
+print(len(list_var))
+print(len(str_var))
+print(len(dict_var))
